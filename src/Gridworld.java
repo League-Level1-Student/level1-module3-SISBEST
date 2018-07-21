56bbbd981b35cd7072abe4b433178507ea70842e
League Student <league@iMac-9.local>
2018-07-21 16:53:30 -0700

    WhackAMole, Gridworld

--- src/Gridworld.java
+++ src/Gridworld.java
@@ -1,14 +1,32 @@
-import info.gridworld.actor.Bug;
-import info.gridworld.grid.Location;
-import info.gridworld.world.World;
+import java.awt.Color;
+import java.util.Random;
+
+import info.gridworld.actor.*;
+import info.gridworld.grid.*;
+import info.gridworld.world.*;
 public class Gridworld {
-	World w = new World();
+	
 	public static void main(String[] args) {
 		Gridworld g = new Gridworld();
 		g.addStuff();
 	}
 	void addStuff(){
+		World w = new World();
+		w.show();
 		Location l1 = new Location(2,2);
 		Bug b = new Bug();
+		w.add(l1, b);
+		Random r = new Random();
+		Location l2 = new Location(r.nextInt(9), r.nextInt(9));
+		Bug b2 = new Bug();
+		b2.setColor(Color.BLUE);
+		b2.turn();
+		w.add(l2, b2);
+		for (int i=0; i<200; i++) {
+			Location l3 = new Location(r.nextInt(9), r.nextInt(9));
+			Flower f2 = new Flower();
+			f2.setColor(Color.BLUE);
+			w.add(l3, f2);
+		}
 	}
 }
--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,17 +1,69 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
+
 import javax.swing.*;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 JFrame f = new JFrame();
 JPanel p = new JPanel();
+Date timeAtStart = new Date();
+int score;
 public static void main(String[] args) {
-	
+	WhackAMole w = new WhackAMole();
+	Random b = new Random();
+	int r = b.nextInt(23);
+	w.drawButtons(r);
 }
 void drawButtons(int rnd) {
 	for(int i = 0; i<24; i++) {
 		JButton b = new JButton();
+		p.add(b);
+		f.add(p);
+		f.setSize(523, 596);
+		f.setVisible(true);
+		b.addActionListener(this);
 		if(rnd == i) {
 			b.setText("MOLE!!!");
 		}
 	}
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	JButton beep = (JButton) e.getSource();
+	if(!(beep.getText().equals("MOLE!!!"))){
+		speak("NO! Score is "+ score);
+	score--;
+	}
+	else {
+		score++;
+		speak("YES! Score is "+ score);
+	}
+	f.remove(p);
+	p = new JPanel();
+	Random r = new Random();
+	drawButtons(r.nextInt(23));
+	
+	if (score == 10) {
+		endGame(timeAtStart, score);
+	}
+	else if (score == -5) {
+		System.err.println("You Lose!");
+		System.exit(1);
+	}
+}
+void speak(String words) {
+    try {
+         Runtime.getRuntime().exec("say " + words).waitFor();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+private void endGame(Date timeAtStart, int molesWhacked) {
+    Date timeAtEnd = new Date();
+    JOptionPane.showMessageDialog(null, "Your whack rate is "
+         + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+         + " moles per second.");
 }
 }
